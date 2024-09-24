77bb898ebf04932cfa0f40cce6728292e3418b5b
18102007liro <165839181+18102007liro@users.noreply.github.com>
2024-09-24 16:39:29 -0300

    Add files via upload

---
+++ perguntas2-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do brasil?',
+    'O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca'
+)
+
+criaCartao(
+    'geografia',
+    'Qual a capital da Noruega?',
+    'A capital da Noruega é Oslo'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual é o planeta mais próximo do sol? ',
+    'O planeta mais próximo do sol é mercúrio'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantos lados tem um hexágono?',
+    'um hexágono tem seis lados'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a obra A última ceia? ',
+    'Leornado da Vinci'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual o planeta conhecido como o planeta vermelho?',
+    'Marte'
+)
